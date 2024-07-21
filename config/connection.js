4a10de989a4da7534befb96614f9e123d4c19b79
Tylerz3936 <tylerzhao103@gmail.com>
2024-07-21 00:41:39 -0400

    other changes

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,17 @@
 require('dotenv').config();
 
+console.log('DB_NAME:', );
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_PORT:', process.env.DB_PORT);
+
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'postgres',
       dialectOptions: {
